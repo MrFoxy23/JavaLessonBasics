fd035f9652d79c054156f79f522c8dd4eb9c2908
Artiom Choults <artiom.choults@pearlchain.net>
2018-01-13 12:56:06 +0100

    privet maksim

---
+++ src/main/java/head/Brain.java
@@ -0,0 +1,8 @@
+package head;
+
+public class Brain {
+    public static void main(String[] args) {
+        Mouth mouth = new Mouth();
+        mouth.say("Privet Maksim!");
+    }
+}
---
+++ src/main/java/head/Mouth.java
@@ -0,0 +1,7 @@
+package head;
+
+public class Mouth {
+    public void say(String word){
+        System.out.print(word);
+    }
+}
