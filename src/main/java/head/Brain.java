96c125544c0a1d5cb9b4ff610773a2511722f1ec
Artiom Choults <artiom.choults@pearlchain.net>
2018-01-13 13:33:15 +0100

    support command line

--- src/main/java/head/Brain.java
+++ src/main/java/head/Brain.java
@@ -2,7 +2,13 @@ package head;
 
 public class Brain {
     public static void main(String[] args) {
+        String default_word = "Maksim";
+
+        if(args.length > 0){
+            default_word = args[0];
+        }
+
         Mouth mouth = new Mouth();
-        mouth.say("Privet Maksim!");
+        mouth.say("Privet " + default_word + "!");
     }
 }
